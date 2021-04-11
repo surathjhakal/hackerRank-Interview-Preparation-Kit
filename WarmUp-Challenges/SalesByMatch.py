2037b255e7b66b9ed948f2ba7b076b81942712ff
Surath Jhakal <46605283+surathjhakal@users.noreply.github.com>
2021-04-11 18:24:28 +0530

    Update SalesByMatch.py

--- WarmUp-Challenges/SalesByMatch.py
+++ WarmUp-Challenges/SalesByMatch.py
@@ -1,12 +1,6 @@
 #!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
 # Complete the sockMerchant function below.
+
 #Complexity is time:O(n) & space=O(n)
 def sockMerchant(n, ar):
     leftSocks=[]
@@ -20,16 +14,3 @@ def sockMerchant(n, ar):
             pairSocks.append(socks)
 
     return len(pairSocks)
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    n = int(input())
-
-    ar = list(map(int, input().rstrip().split()))
-
-    result = sockMerchant(n, ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
