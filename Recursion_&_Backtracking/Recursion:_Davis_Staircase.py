10b5de2ce7ed325d449803d86fbd7620b23189d1
Surath Jhakal <46605283+surathjhakal@users.noreply.github.com>
2021-04-27 21:25:02 +0530

    Create Recursion:_Davis_Staircase.py

---
+++ Recursion_&_Backtracking/Recursion:_Davis_Staircase.py
@@ -0,0 +1,11 @@
+result=dict()
+result[1]=1
+result[2]=2
+result[3]=4
+def stepPerms(n):
+    if n==0:
+        return 1
+    if n in result.keys():
+        return result[n]
+    result[n]=stepPerms(n-1)+stepPerms(n-2)+stepPerms(n-3)
+    return result[n]
